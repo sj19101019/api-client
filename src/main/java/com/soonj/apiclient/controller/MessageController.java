766147761adeef7656be3ebf719548aa23e30eb4
jeff.hu <sj19101019@gmail.com>
2020-02-09 13:52:13 +0800

    init

---
+++ src/main/java/com/soonj/apiclient/controller/MessageController.java
@@ -0,0 +1,18 @@
+package com.soonj.apiclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class MessageController {
+	@Value("${message}")
+	private String message;
+
+	@RequestMapping("/message")
+	public String message() {
+		return this.message;
+	}
+}
