6d15b73995314cb3642aae9630b0917d07c1691a
jeff_hu <jeff@kuma-gaming.com>
2020-02-10 17:11:28 +0800

    add url

--- src/main/java/com/soonj/apiclient/controller/MessageController.java
+++ src/main/java/com/soonj/apiclient/controller/MessageController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RefreshScope
 @RestController
+@RequestMapping("message")
 public class MessageController {
 	@Value("${message}")
 	private String message;
